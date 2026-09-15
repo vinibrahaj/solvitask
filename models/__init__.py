212b0b4ca398da8ea9d326d22c7f88608aca770e
vinibrahaj <ervin.brh@gmail.com>
2026-09-15 15:50:11 +0200

    rebase

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import customer
-from . import worker
-from . import service
-from . import tool
-from . import material
-from . import job_stage
-from . import job
--- models/tool.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class SolvitaskTool(models.Model):
-    _name = 'solvitask.tool'
-    _description = 'Tool'
-
-    name = fields.Char(string='Tool', required=True)
-    description = fields.Char(string='Description')
