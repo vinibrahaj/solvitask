59d94071a0155b22a03d34abfd4ec01763ff1b17
Ervin <ervin.brh@gmail.com>
2026-09-15 15:58:07 +0200

    Resolve import conflicts in __init__.py
    Removed conflicting import statements for requests and job_request.

--- models/__init__.py
+++ models/__init__.py
@@ -3,8 +3,5 @@ from . import worker
 from . import service
 from . import job_stage
 from . import job
-<<<<<<< HEAD
 from . import requests
-=======
 from . import job_request
->>>>>>> 363dfc8b76f846a74b25bc46b5cb73d0d99f3377
